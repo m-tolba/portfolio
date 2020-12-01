f741117619eeb4b2aab31af026dd059eed0936bc
m-tolba <mariam.tolba1@gmail.com>
2020-12-01 17:37:07 -0600

    Update jquery.scroll.js

--- jquery.scroll.js
+++ jquery.scroll.js
@@ -1,16 +1,4 @@
-var input = document.getElementById("project2");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("project2").click();
-  }
-});
-
+//smoothly transitions from top of main page down to view projects 
   $('.js-anchor-link').click(function(e){
   e.preventDefault();
   var target = $($(this).attr('href'));
